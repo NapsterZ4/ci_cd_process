f4dbed66185c4ca5212e24258f1f2ef67d108e0b
Jorge <jorge.zapata>
2026-03-09 21:12:56 -0600

    new api health

--- api/routers/prediction.py
+++ api/routers/prediction.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-
+from fastapi.responses import JSONResponse
+from fastapi import status
 from api.schemas.prediction import PredictionRequest, PredictionResponse
 from api.services.model_service import model_service
 
@@ -10,3 +11,11 @@ router = APIRouter(prefix="/predict", tags=["Prediccion"])
 def predict(request: PredictionRequest) -> PredictionResponse:
     clase, prediccion = model_service.predict(request.model_dump())
     return PredictionResponse(prediccion=prediccion, clase=clase)
+
+
+@router.get("/health", tags=["Health"])
+def health() -> JSONResponse:
+    return JSONResponse(
+        content={"status": "ok"},
+        status_code=status.HTTP_200_OK
+    )
